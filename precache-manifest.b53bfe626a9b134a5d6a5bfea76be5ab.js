92024cbc7ff74d656313623e1ccbf80a7e52bdc3
sathish <sathishct2013@gmail.com>
2020-03-04 02:47:34 +0530

    Updates

---
+++ precache-manifest.b53bfe626a9b134a5d6a5bfea76be5ab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c4bac5f753a0f58734f0f1ccf766579",
+    "url": "/money-manager/index.html"
+  },
+  {
+    "revision": "81f563e39766b590d066",
+    "url": "/money-manager/static/css/main.84b95dce.chunk.css"
+  },
+  {
+    "revision": "a4d9ceafa6cd1bca7506",
+    "url": "/money-manager/static/js/2.edf2b198.chunk.js"
+  },
+  {
+    "revision": "ac9fac6c8e755a8efecd01981adb88e6",
+    "url": "/money-manager/static/js/2.edf2b198.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "81f563e39766b590d066",
+    "url": "/money-manager/static/js/main.09635bd0.chunk.js"
+  },
+  {
+    "revision": "e215a5a6b2dafaab4d39",
+    "url": "/money-manager/static/js/runtime-main.a29d5fea.js"
+  }
+]);
